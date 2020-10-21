8125fcc109ea64c4bfa964e685a885d8cddc98da
Voryzen <piro.travesura@gmail.com>
2020-10-21 15:32:38 +1000

    chore(core): remove extra junk/ unneeded code (#113)
    * Delete git_sync.sh
    
    * Delete lint.sh
    
    * Delete docker_helper.sh
    
    * Delete generate_ansible_pass.sh
    
    Co-authored-by: Denis Evers <denis-ev@users.noreply.github.com>

--- generate_ansible_pass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  function sha256sum() { shasum -a 256 "$@" ; } && export -f sha256sum
-fi
-date +%s | sha256sum | base64 | head -c 32  > ~/.vlab_vault_pass
--- lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install yamllint
-find . -type f -name '*.yml' | sed 's|\./||g' | egrep -v '(\.kitchen/|\[warning\]|\.molecule/)' | xargs yamllint -c yamllint.conf -f parsable
